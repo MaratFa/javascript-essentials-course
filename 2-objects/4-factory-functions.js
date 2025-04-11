2297353f9aa12e357969e926449ae3f145a0d8bd
MaratFa <evolution99@rambler.ru>
2025-04-11 07:05:49 +0300

    Edited

--- 2-objects/4-factory-functions.js
+++ 2-objects/4-factory-functions.js
@@ -17,8 +17,32 @@ function createProgrammer(name, preferredLanguage) {
       console.log(`${this.name} writes ${this.preferredLanguage} code.`);
     },
     drinkCoffe() {
-      
-    }
+      console.log(`$(this.name) drink coffee.`);
+    },
   };
 }
 
+const newProgrammer = createProgrammer("Alice", "JavaScript");
+newProgrammer.writeCode();
+
+// Exercise
+const groceryItem = {
+  name: "Apples",
+  quantity: 4,
+  display() {
+    console.log(`${this.quantity} x ${this.name}`);
+  },
+};
+
+function createGroceryItem(name, quantity) {
+  return {
+    name,
+    quantity,
+    display() {
+      console.log(`${this.quantity} x ${this.name}`);
+    },
+  };
+}
+
+const newItem = createGroceryItem("Bananas", 5);
+newItem.display();
