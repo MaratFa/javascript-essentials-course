865e27fb41a5f61ce9208c23f163716dbfdc606e
MaratFa <evolution99@rambler.ru>
2025-04-14 00:08:18 +0300

    Edited

--- 2-objects/7-functions-are-objects.js
+++ 2-objects/7-functions-are-objects.js
@@ -18,12 +18,33 @@ const n = add;
 console.log(n(2, 2));
 console.log(add.length);
 
-const ProgrammerFunc = new Function('name', `
+const ProgrammerFunc = new Function(
+  "name",
+  `
   this.name = name;
   this.writeCode = function() {
     console.log('Code in JavaScript');
   }  
-`);
+`
+);
 
-const programmer = new ProgrammerFunc('Steven');
-console.log(programmer);
+const programmer = new ProgrammerFunc("Steven");
+programmer.writeCode();
+
+// Exercise
+function GroceryItem(name, quantity) {
+  this.name = name;
+  this.quantity = quantity;
+  this.display = function () {
+    console.log(`${this.quantity} x ${this.name}`);
+  };
+}
+
+const newItem = new GroceryItem("Banana", 4);
+
+function calculatePrice(groceryItem, price) {
+  return price * groceryItem.quantity;
+}
+
+const performCalculation = calculatePrice;
+console.log(performCalculation(newItem, 3));
