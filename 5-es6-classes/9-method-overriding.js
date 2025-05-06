e9ddd10ea17ff61bb273fe3845699d2d45017f6e
MaratFa <evolution99@rambler.ru>
2025-05-06 06:44:33 +0300

    Added

---
+++ 5-es6-classes/9-method-overriding.js
@@ -0,0 +1,25 @@
+class Programmer {
+  constructor(name) {
+    this.name = name;
+  }
+
+  work() {
+    console.log(`${this.name} is solving problems.`);
+  }
+}
+
+class FrontEndProgrammer extends Programmer {
+  constructor(name) {
+    super(name); // Initialize the base class part of the object
+  }
+
+  work() {
+    super.work(); // Calls the base class method
+    console.log(
+      `${this.name} is also designing and implementing UI Components.`
+    );
+  }
+}
+
+const john = new FrontEndProgrammer("John");
+john.work();
