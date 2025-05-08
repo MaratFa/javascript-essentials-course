703e57ad50db1d9a67bed24bc245c685389d05dc
MaratFa <evolution99@rambler.ru>
2025-05-08 09:53:30 +0300

    Added

---
+++ 6-es6-tooling/3-webpack/src/index.js
@@ -0,0 +1,22 @@
+export class Programmer {
+  role = "developer";
+
+  #experience;
+
+  constructor(name, technology, experience) {
+    this.name = name;
+    this.technology = technology;
+    this.#experience = experience;
+  }
+
+  develop() {
+    console.log(`${this.name} develops software using ${this.technology} with ${this.#experience} years of experience.`);
+  }
+
+  getExperience() {
+    return this.#experience;
+  }
+}
+
+const dev = new Programmer('John', 'JavaScript', 11);
+dev.develop();
