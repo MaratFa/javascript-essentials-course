40261114c46d7b14976982472da59443d7d64818
MaratFa <evolution99@rambler.ru>
2025-05-08 09:53:17 +0300

    Added

---
+++ 6-es6-tooling/2-babel/index.js
@@ -0,0 +1,19 @@
+export class Programmer {
+  role = "developer";
+
+  #experience;
+
+  constructor(name, technology, experience) {
+    this.name = name;
+    this.technology = technology;
+    this.#experience = experience;
+  }
+
+  develop() {
+    console.log(`${this.name} develops software using ${this.technology} with ${this.#experience} years of experience.`);
+  }
+
+  getExperience() {
+    return this.#experience;
+  }
+}
