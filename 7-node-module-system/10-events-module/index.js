1be110cfac7a1e7240fb3b2e79ce8de1a29c6742
MaratFa <evolution99@rambler.ru>
2025-05-10 00:09:56 +0300

    Added

---
+++ 7-node-module-system/10-events-module/index.js
@@ -0,0 +1,10 @@
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
+
+emitter.on('messageLogged', (arg) => {
+  console.log('Listener called', arg);  
+});
+
+emitter.emit('messageLogged', { id: 1, url: 'example.com'});
+
