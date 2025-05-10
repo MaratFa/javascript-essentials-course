ec7f35b72d68276d56703c16d5b195f743391f24
MaratFa <evolution99@rambler.ru>
2025-05-11 00:42:58 +0300

    Added

---
+++ 8-node-package-manager/3-using-a-package/index.js
@@ -0,0 +1,8 @@
+const _ = require("underscore");
+
+// const isEven = _.some([1, 2, 3, 4, 5], (num) => num % 2 === 0);
+// console.log(isEven);
+
+const doesContain = _.contains([1, 2, 3], 4);
+console.log(doesContain);
+
